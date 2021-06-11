f462a7a9ab95b82d0f745bddc1120b396a4bc3a4
vogr <contact@vogier.fr>
2021-06-11 15:11:36 +0000

    Run tests in R, Rsh and Rsh+contextual profiler

--- runtests/runtests.sh
+++ runtests/runtests.sh
@@ -4,15 +4,72 @@ set -u
 CWD="$(dirname "$(readlink -f "$0")")"
 cd "$CWD" || exit 1
 
-export R="$HOME/bin/Rmrscript"
+export R_LIBS="$HOME/.Renv/versions/3.6.2/lib/R/library"
 
 
-find tests -mindepth 1 -maxdepth 1 -type d -print0 |
-  sort -z |
-  xargs -P4 -r0 -n1 bash -c '
-cd $1 &&
-mkdir -p profile &&
-printf "running %s...\n" "$1"
-export CONTEXT_LOGS=true PIR_DEBUG=ShowWarnings
-timeout 10m $R -f main.R > run.log 2>&1
-' xargs-sh
+process_test() {
+  if [[ $1 =~ ^#.* ]]; then 
+    return;
+  fi
+
+  PKG="$(realpath "$1")"
+  PKG_NAME="$(basename "$PKG")"
+  TEST="$(realpath "$2")"
+  TEST_NAME="$(basename "$TEST")"
+
+  printf "* Test %s\n" "$TEST"
+    
+  TEST_REL="$(realpath --relative-to="$PKG" "$TEST")"
+  TREE="${TEST_REL%.*}"
+  DEST="tests/$PKG_NAME/$TREE"
+
+  printf "  into %s\n" "$DEST"
+
+  mkdir -p "$DEST" &&
+  (
+    cd "$DEST" &&
+
+    # Run the file:
+    #   with R (for benchmark) x10
+    #   with Rjr (for benchmark) x10
+    #   with Rmr (for context logging)
+
+    printf "  + benchmark R...\n"
+    mkdir -p bench-R &&
+    (
+      for i in {1..10}; do
+        Rscript "$TEST" "bench-R/bench-$i.RDS" || break
+      done
+    ) > R.log 2>&1
+
+    printf "  + benchmark Rsh...\n"
+    mkdir -p bench-R &&
+    mkdir -p bench-Rsh &&
+    (
+      for i in {1..10}; do
+        "$HOME/bin/Rjrscript" "$TEST" "bench-Rsh/bench-$i.RDS" || break
+      done
+    ) > Rsh.log 2>&1
+
+    printf "  + context profile...\n"
+    mkdir "profile" &&
+    "$HOME/bin/Rmrscript" "$TEST" > Rm.log 2>&1
+  )
+}
+export -f process_test
+
+process_pkg () {
+  PKG="$(realpath "$1")"
+  PKGNAME="$(basename "$PKG")"
+
+  printf "Processing %s...\n" "$PKGNAME"
+
+  find "$PKG" -type f -name "*.R" -print0 |
+      sort -z |
+      xargs -r0 -n1 bash -c 'process_test "$@"' _ "$PKG"
+}
+export -f process_pkg
+
+xargs -r -n1 bash -c 'process_pkg "$@"' _ < packages.txt
+
+printf "Done\n" > /dev/stderr
