154cd91ab69d7dc65b7fc4be5da7d8d093a75876
vogr <contact@vogier.fr>
2021-06-18 14:45:48 +0000

    More stats

--- make_stats/stats.R
+++ make_stats/stats.R
@@ -15,7 +15,7 @@ failures <- df %>% filter(SUCCESS == FALSE)
 write_csv(successes, "successes.csv")
 write_csv(failures, "failures.csv")
 
-write_csv(arrange(failures, desc(CMP_TIME)), "failure_desc.csv")
+write_csv(arrange(failures, desc(CMP_TIME)), "failure_cmp_time.csv")
 
 write_csv(arrange(df, desc(CMP_TIME)), "cmp_times.csv")
 
@@ -24,5 +24,13 @@ main_fun <- df %>% filter(NAME == paste0(test_pkg, ":::", test_fun))
 write_csv(main_fun, "main_fun.csv")
 
 
-recompilation <- df %>% filter(ID_CMP > 0 && SUCCESS == TRUE)
+recompilation <- df %>% filter(ID_CMP > 0 & SUCCESS == TRUE) %>% arrange(desc(ID_CMP), test_pkg, test_fun, test_fname)
 write_csv(recompilation, "recompilation.csv")
+
+
+# How many different versions?
+
+versions <- df %>% group_by(NAME) %>%
+             summarise(n_versions = length(unique(VERSION))) %>%
+             arrange(desc(n_versions))
+write_csv(versions, "versions.csv")
