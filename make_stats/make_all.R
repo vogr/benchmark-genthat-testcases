80dcb98248c1b917771dfa3352210471ca9f9190
vogr <contact@vogier.fr>
2021-06-20 15:32:01 +0000

    Use data.table for faster processing

--- make_stats/make_all.R
+++ make_stats/make_all.R
@@ -1,16 +1,13 @@
 #!/usr/bin/env Rscript
 
-library(tibble)
-library(dplyr)
-library(readr)
+library(data.table)
 
 TESTS_DIR <- "../runtests/tests"
+#TESTS_DIR <- "devel"
 
 data_ready_dirs <- normalizePath(dirname(dir(TESTS_DIR, pattern = "DATA_READY", recursive=TRUE, full.names=TRUE)))
 
-
 message("Merge data from ", length(data_ready_dirs), " test runs.")
-
 
 read_and_prepare <- function(d) {
 
@@ -19,11 +16,14 @@ read_and_prepare <- function(d) {
   test_pkg <- basename(dirname(dirname(d)))
 
   # ID is a size_t and can be larger than R int type, so use a string
-  read_csv(file.path(d, "profile", "compile_stats.csv"), col_types="cccild") %>%
-    mutate(test_pkg=test_pkg, test_fun=test_fun, test_fname=test_fname, .before="ID")
+  classes <- c("character", "character", "character", "integer", "integer", "double")
+  dt <- fread(file=file.path(d, "profile", "compile_stats.csv"), header=TRUE, colClasses=classes)
+  dt <- data.table(test_pkg=test_pkg, test_fun=test_fun, test_fname=test_fname, dt)
+  dt
 }
 
 # Bind all csv.s together
-df <- bind_rows(lapply(data_ready_dirs, read_and_prepare))
+df <- rbindlist(lapply(data_ready_dirs, read_and_prepare))
 
-write_csv(df, "all.csv")
+fwrite(df, file="all.csv")
+saveRDS(df, file="all.RDS", compress=FALSE)
--- make_stats/stats.R
+++ make_stats/stats.R
@@ -1,30 +1,38 @@
 #!/usr/bin/env Rscript
 
+
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
+
 library(tibble)
-library(dplyr)
 library(readr)
 library(ggplot2)
 
-df <- read_csv("all.csv", col_types="ccccccild")
-
+dt <- readRDS("all.RDS")
+df <- lazy_dt(dt)
 
 successes <- df %>% filter(SUCCESS == TRUE)
 failures <- df %>% filter(SUCCESS == FALSE)
 
 
-write_csv(successes, "successes.csv")
-write_csv(failures, "failures.csv")
+write_csv(as.data.table(successes), "successes.csv")
+write_csv(as.data.table(failures), "failures.csv")
 
-write_csv(arrange(failures, desc(CMP_TIME)), "failure_cmp_time.csv")
+write_csv(as.data.table(arrange(failures, desc(CMP_TIME))), "failure_cmp_time.csv")
 
-write_csv(arrange(df, desc(CMP_TIME)), "cmp_times.csv")
+write_csv(as.data.table(arrange(df, desc(CMP_TIME))), "cmp_times.csv")
 
 main_fun <- df %>% filter(NAME == paste0(test_pkg, ":::", test_fun))
+  %>% as.data.table()
 
 write_csv(main_fun, "main_fun.csv")
 
 
-recompilation <- df %>% filter(ID_CMP > 0 & SUCCESS == TRUE) %>% arrange(desc(ID_CMP), test_pkg, test_fun, test_fname)
+recompilation <- df %>% filter(ID_CMP > 1 & SUCCESS == TRUE) %>%
+  arrange(desc(ID_CMP), test_pkg, test_fun, test_fname) %>%
+  as.data.table()
+
 write_csv(recompilation, "recompilation.csv")
 
 
@@ -33,4 +41,5 @@ write_csv(recompilation, "recompilation.csv")
 versions <- df %>% group_by(NAME) %>%
              summarise(n_versions = length(unique(VERSION))) %>%
              arrange(desc(n_versions))
+           %>% as.data.table()
 write_csv(versions, "versions.csv")
